df7f257ec9fd94369f1b34c78e7db8233842d4c3
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-02-25 01:18:59 +0530

    first commit

--- privacy.php
+++ privacy.php
@@ -23,31 +23,33 @@ global $conn, $system_techs;
 </head>
 
 <body class="body">
-    <div class="uk-container">
-        <h2>Privacy Policy</h2>
-        <p>At Xpeed Technologies, we are committed to protecting the privacy of our clients and website visitors. This Privacy Policy outlines how we collect, use, and disclose your personal information.</p>
+    <section class="uk-section">
+        <div class="uk-container">
+            <h2>Privacy Policy</h2>
+            <p>At Xpeed Technologies, we are committed to protecting the privacy of our clients and website visitors. This Privacy Policy outlines how we collect, use, and disclose your personal information.</p>
 
-        <h3>Information We Collect</h3>
-        <p>We may collect personal information from you when you contact us, register for our services, or visit our website. This information may include your name, email address, phone number, and company name.</p>
+            <h3>Information We Collect</h3>
+            <p>We may collect personal information from you when you contact us, register for our services, or visit our website. This information may include your name, email address, phone number, and company name.</p>
 
-        <h3>How We Use Your Information</h3>
-        <p>We use your personal information to provide our services, respond to your inquiries, and improve our website and services. We may also use your information for marketing purposes, such as sending you newsletters or promotional offers. You can opt-out of receiving marketing communications at any time.</p>
+            <h3>How We Use Your Information</h3>
+            <p>We use your personal information to provide our services, respond to your inquiries, and improve our website and services. We may also use your information for marketing purposes, such as sending you newsletters or promotional offers. You can opt-out of receiving marketing communications at any time.</p>
 
-        <h3>How We Protect Your Information</h3>
-        <p>We take appropriate measures to protect your personal information from unauthorized access, use, or disclosure. We use industry-standard security measures to safeguard your data, including encryption and secure servers.</p>
+            <h3>How We Protect Your Information</h3>
+            <p>We take appropriate measures to protect your personal information from unauthorized access, use, or disclosure. We use industry-standard security measures to safeguard your data, including encryption and secure servers.</p>
 
-        <h3>Disclosure of Your Information</h3>
-        <p>We may disclose your personal information to third-party service providers who assist us in providing our services. We may also disclose your information if required by law or to protect our legal rights.</p>
+            <h3>Disclosure of Your Information</h3>
+            <p>We may disclose your personal information to third-party service providers who assist us in providing our services. We may also disclose your information if required by law or to protect our legal rights.</p>
 
-        <h3>Your Rights</h3>
-        <p>You have the right to access, update, or delete your personal information. You can also request that we restrict the processing of your data or object to our use of your information. To exercise these rights, please contact us using the contact information provided below.</p>
+            <h3>Your Rights</h3>
+            <p>You have the right to access, update, or delete your personal information. You can also request that we restrict the processing of your data or object to our use of your information. To exercise these rights, please contact us using the contact information provided below.</p>
 
-        <h3>Changes to Our Privacy Policy</h3>
-        <p>We may update our Privacy Policy from time to time. Any changes will be posted on this page, and we encourage you to review this Privacy Policy periodically.</p>
+            <h3>Changes to Our Privacy Policy</h3>
+            <p>We may update our Privacy Policy from time to time. Any changes will be posted on this page, and we encourage you to review this Privacy Policy periodically.</p>
 
-        <h3>Contact Us</h3>
-        <p>If you have any questions or concerns about our Privacy Policy, please contact us at privacy@codecrafters.com.</p>
-    </div>
+            <h3>Contact Us</h3>
+            <p>If you have any questions or concerns about our Privacy Policy, please contact us at privacy@codecrafters.com.</p>
+        </div>
+    </section>
 </body>
 
 </html>
--- terms.php
+++ terms.php
@@ -23,42 +23,44 @@ global $conn, $system_techs;
 </head>
 
 <body class="body">
-    <div class="uk-container">
-        <h2>Terms and Conditions</h2>
+    <section class="uk-section">
+        <div class="uk-container">
+            <h2>Terms and Conditions</h2>
 
-        <p>Welcome to Xpeed Technologies! These Terms and Conditions govern your use of our website and services. By using our website or services, you agree to these Terms and Conditions.</p>
+            <p>Welcome to Xpeed Technologies! These Terms and Conditions govern your use of our website and services. By using our website or services, you agree to these Terms and Conditions.</p>
 
-        <h3>1. Definitions
-        <p>"Xpeed Technologies," "we," "us," or "our" refers to our web development and app development company.</p>
+            <h3>1. Definitions
+                <p>"Xpeed Technologies," "we," "us," or "our" refers to our web development and app development company.</p>
 
-        <p>"Client" or "you" refers to any person or entity who uses our website or services.</p>
+                <p>"Client" or "you" refers to any person or entity who uses our website or services.</p>
 
-        <p>"Services" refers to our web development and app development services.</p>
+                <p>"Services" refers to our web development and app development services.</p>
 
-        <h3>2. Use of Our Website and Services</h3>
-        <p>You may use our website and services for lawful purposes only. You may not use our website or services to transmit any illegal or harmful content, or to infringe upon the rights of others. We reserve the right to terminate your access to our website or services if we suspect any misuse.</p>
+                <h3>2. Use of Our Website and Services</h3>
+                <p>You may use our website and services for lawful purposes only. You may not use our website or services to transmit any illegal or harmful content, or to infringe upon the rights of others. We reserve the right to terminate your access to our website or services if we suspect any misuse.</p>
 
-        <h3>3. Intellectual Property</h3>
-        <p>All content on our website and related to our services, including text, images, logos, and software, is the property of Xpeed Technologies or our licensors. You may not use any of our content without our prior written consent.</p>
+                <h3>3. Intellectual Property</h3>
+                <p>All content on our website and related to our services, including text, images, logos, and software, is the property of Xpeed Technologies or our licensors. You may not use any of our content without our prior written consent.</p>
 
-        <h3>4. Confidentiality</h3>
-        <p>We may access and use your confidential information to provide our services to you. We will maintain the confidentiality of your information and will not disclose it to any third party without your consent.</p>
+                <h3>4. Confidentiality</h3>
+                <p>We may access and use your confidential information to provide our services to you. We will maintain the confidentiality of your information and will not disclose it to any third party without your consent.</p>
 
-      
-        <h3>5. Indemnification</h3>
-        <p>You agree to indemnify and hold us harmless from any claims, damages, or losses arising out of your use of our website or services.</p>
 
-        <h3>6. Termination</h3>
-        <p>We may terminate your access to our website or services at any time for any reason. You may terminate your use of our services by providing us with written notice.</p>
+                <h3>5. Indemnification</h3>
+                <p>You agree to indemnify and hold us harmless from any claims, damages, or losses arising out of your use of our website or services.</p>
 
-        <h3>7. Governing Law</h3>
-        <p>These Terms and Conditions are governed by the laws of the state in which we are incorporated.</p>
+                <h3>6. Termination</h3>
+                <p>We may terminate your access to our website or services at any time for any reason. You may terminate your use of our services by providing us with written notice.</p>
 
-        <h3>8. Modification</h3>
-        <p>We may modify these Terms and Conditions at any time. Any changes will be posted on our website, and your continued use of our website or services after the changes become effective constitutes your acceptance of the new Terms and Conditions.</p>
+                <h3>7. Governing Law</h3>
+                <p>These Terms and Conditions are governed by the laws of the state in which we are incorporated.</p>
 
-        <p>If you have any questions or concerns about these Terms and Conditions, please contact us at info@xpeed.co.in.</p>
-    </div>
+                <h3>8. Modification</h3>
+                <p>We may modify these Terms and Conditions at any time. Any changes will be posted on our website, and your continued use of our website or services after the changes become effective constitutes your acceptance of the new Terms and Conditions.</p>
+
+                <p>If you have any questions or concerns about these Terms and Conditions, please contact us at info@xpeed.co.in.</p>
+        </div>
+    </section>
 </body>
 
 </html>
