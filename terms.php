bdaf3e89436593389b9676ceb013ea5f9888891e
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-02-25 01:18:02 +0530

    first commit

---
+++ terms.php
@@ -0,0 +1,64 @@
+<?
+
+include  "vendor/autoload.php";
+include("back/config.php");
+global $conn, $system_techs;
+?>
+
+<html lang="en-gb">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/uikit.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.11.0/devicon.min.css">
+
+    <link rel="icon" href="assets/images/logo.png">
+
+    <script src="js/uikit.js"></script>
+    <script src="js/uikit-icons.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
+
+    <title>Xpeed Technologies: Custom Web and App Development Services for Your Business.</title>
+</head>
+
+<body class="body">
+    <div class="uk-container">
+        <h2>Terms and Conditions</h2>
+
+        <p>Welcome to Xpeed Technologies! These Terms and Conditions govern your use of our website and services. By using our website or services, you agree to these Terms and Conditions.</p>
+
+        <h3>1. Definitions
+        <p>"Xpeed Technologies," "we," "us," or "our" refers to our web development and app development company.</p>
+
+        <p>"Client" or "you" refers to any person or entity who uses our website or services.</p>
+
+        <p>"Services" refers to our web development and app development services.</p>
+
+        <h3>2. Use of Our Website and Services</h3>
+        <p>You may use our website and services for lawful purposes only. You may not use our website or services to transmit any illegal or harmful content, or to infringe upon the rights of others. We reserve the right to terminate your access to our website or services if we suspect any misuse.</p>
+
+        <h3>3. Intellectual Property</h3>
+        <p>All content on our website and related to our services, including text, images, logos, and software, is the property of Xpeed Technologies or our licensors. You may not use any of our content without our prior written consent.</p>
+
+        <h3>4. Confidentiality</h3>
+        <p>We may access and use your confidential information to provide our services to you. We will maintain the confidentiality of your information and will not disclose it to any third party without your consent.</p>
+
+      
+        <h3>5. Indemnification</h3>
+        <p>You agree to indemnify and hold us harmless from any claims, damages, or losses arising out of your use of our website or services.</p>
+
+        <h3>6. Termination</h3>
+        <p>We may terminate your access to our website or services at any time for any reason. You may terminate your use of our services by providing us with written notice.</p>
+
+        <h3>7. Governing Law</h3>
+        <p>These Terms and Conditions are governed by the laws of the state in which we are incorporated.</p>
+
+        <h3>8. Modification</h3>
+        <p>We may modify these Terms and Conditions at any time. Any changes will be posted on our website, and your continued use of our website or services after the changes become effective constitutes your acceptance of the new Terms and Conditions.</p>
+
+        <p>If you have any questions or concerns about these Terms and Conditions, please contact us at info@xpeed.co.in.</p>
+    </div>
+</body>
+
+</html>
